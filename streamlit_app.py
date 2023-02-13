f52ff12ef61e8ff2a08a02d577ed2e643b45adce
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-13 22:46:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,24 +41,10 @@ def BarcodeReader(image):
         return st.write("Barcode Not Detected or your barcode is blank/corrupted!")
         
     else:
+        return f"{detectedBarcodes.data}"
     
-        # Traverse through all the detected barcodes in image
-        for barcode in detectedBarcodes:
-            
-            # Locate the barcode position in image
-            (x, y, w, h) = barcode.rect
-    
-            # Put the rectangle in image using
-            # cv2 to highlight the barcode
-            cv2.rectangle(cv2_img, (x-10, y-10),
-                        (x + w+10, y + h+10),
-                        (255, 0, 0), 2)
-            
-            if barcode.data!="":
 
-            # Print the barcode data
-#                 st.write(f"the bar code is {barcode.data}")
-                return f"{barcode.data}"
+                
 
     
 # ---APP---
@@ -69,27 +55,6 @@ title = st.text_input('Movie title', 'insert a title here ...')
 genre = st.multiselect("Genre?", ('Comedy', 'Drama', 'Documentary'))    
 price = st.number_input('Price')
 picture = st.camera_input("Take a picture")
-
-import requests
-
-st.title( 'Mobile Camera Preview in Streamlit' )
-frame_window = st.image( [] )
-take_picture_button = st.button( 'Take Picture' )
-
-while True:
-    # Request the image from the server
-    response = requests.get(url="http://<network_ip_address>:<port>/photo.jpg")
-    imgNp = np.array(bytearray(response.content), dtype=np.uint8)
-    frame = cv2.imdecode(imgNp, cv2.IMREAD_UNCHANGED )
-    # As OpenCV decodes images in BGR format, we'd convert it to the RGB format
-    frame = cv2.cvtColor( frame , cv2.COLOR_BGR2RGB )
-
-    frame_window.image(frame)
-
-    if take_picture_button:
-        # Pass the frame to a model
-        # And show the output here...
-        break
 
 if picture:
     df_dict = {"Kind":kind,"Title":title,"Genre":genre,"Price":price,"Barcode":BarcodeReader(picture)}
