5a2a053fbaa49150449fccc8b8a8b8c9ad2666c5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-13 13:59:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,20 +25,12 @@ def BarcodeReader(image):
     # If not detected then print the message
     if not detectedBarcodes:
         st.write("Barcode Not Detected or your barcode is blank/corrupted!")
+        
     else:
     
         # Traverse through all the detected barcodes in image
         for barcode in detectedBarcodes:
-    
-#             # Locate the barcode position in image
-#             (x, y, w, h) = barcode.rect
-    
-#             # Put the rectangle in image using
-#             # cv2 to highlight the barcode
-#             cv2.rectangle(cv2_img, (x-10, y-10),
-#                         (x + w+10, y + h+10),
-#                         (255, 0, 0), 2)
-
+            
             if barcode.data!="":
 
             # Print the barcode data
