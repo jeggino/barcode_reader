37d2e7a99d16ac0a0e4aaa50ea638231088e0951
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-13 13:46:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,12 @@ def BarcodeReader(image):
 
     # read the image in numpy array using cv2
 #     img = cv2.imread(image)
-    img = np.array(image)
+#     img = np.array(image)
+    bytes_data = image.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
     # Decode the barcode image
-    detectedBarcodes = decode(img)
+    detectedBarcodes = decode(cv2_img)
 
     # If not detected then print the message
     if not detectedBarcodes:
@@ -29,7 +31,7 @@ def BarcodeReader(image):
     
             # Put the rectangle in image using
             # cv2 to highlight the barcode
-            cv2.rectangle(img, (x-10, y-10),
+            cv2.rectangle(cv2_img, (x-10, y-10),
                         (x + w+10, y + h+10),
                         (255, 0, 0), 2)
 
