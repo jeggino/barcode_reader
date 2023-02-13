c56263c4ed2a7c7aaa874b5694c4af9683ffd515
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-13 22:32:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,6 +70,27 @@ genre = st.multiselect("Genre?", ('Comedy', 'Drama', 'Documentary'))
 price = st.number_input('Price')
 picture = st.camera_input("Take a picture")
 
+import requests
+
+st.title( 'Mobile Camera Preview in Streamlit' )
+frame_window = st.image( [] )
+take_picture_button = st.button( 'Take Picture' )
+
+while True:
+    # Request the image from the server
+    response = requests.get(url="http://<network_ip_address>:<port>/photo.jpg")
+    imgNp = np.array(bytearray(response.content), dtype=np.uint8)
+    frame = cv2.imdecode(imgNp, cv2.IMREAD_UNCHANGED )
+    # As OpenCV decodes images in BGR format, we'd convert it to the RGB format
+    frame = cv2.cvtColor( frame , cv2.COLOR_BGR2RGB )
+
+    frame_window.image(frame)
+
+    if take_picture_button:
+        # Pass the frame to a model
+        # And show the output here...
+        break
+
 if picture:
     df_dict = {"Kind":kind,"Title":title,"Genre":genre,"Price":price,"Barcode":BarcodeReader(picture)}
     df = pd.DataFrame(df_dict)
